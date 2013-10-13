5de5489b300f7062c6bee9c1f9b08f693499ff54
daisukewi <d.flamencog@gmail.com>
2013-10-13 20:20:18 +0200

    Add shoot functionality. Needs an arrow to be drawn when shooting

--- Assets/Scripts/TouchControl.cs
+++ Assets/Scripts/TouchControl.cs
@@ -3,16 +3,49 @@ using System.Collections;
 
 public class TouchControl : MonoBehaviour
 {
-
+	private BoxCollider boxCollider;
+	private bool shutting = false;
+	
+	public GameObject Candy;
+	public float ShootForce = 400.0f;
+	
 	// Use this for initialization
 	void Start ()
 	{
-	
+		boxCollider = (BoxCollider)GetComponent(typeof(BoxCollider));
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		GameObject CandyInstance;
+		
+		if (Candy == null)
+			return;
+		
+		if (Input.GetMouseButtonDown (0)) {
+			
+			Vector3 intersectVect = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			intersectVect.z = transform.position.z;
+			
+			//Draw here the arrow
+			Debug.DrawLine (transform.position, intersectVect, Color.red, 0, false);
+			
+			if (!shutting && boxCollider.bounds.Contains (intersectVect)) {
+				shutting = true;
+			}
+		}
+		
+		if (Input.GetMouseButtonUp (0) && shutting) {
+			
+			CandyInstance = Instantiate (Candy, transform.position, transform.rotation) as GameObject;
+			Physics.IgnoreCollision (CandyInstance.collider, collider);
+			
+			Vector3 intersectVect = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			intersectVect.z = transform.position.z;
+			
+			((Rigidbody)CandyInstance.GetComponent (typeof(Rigidbody))).AddForce ((transform.position - intersectVect) * ShootForce);
+			shutting = false;
+		}
 	}
 }
